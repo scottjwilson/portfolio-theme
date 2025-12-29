c94d1da908e08dbcf36381cfcf785dd3896e6711
Scott Wilson <scottmba@Scotts-MacBook-Air.local>
2025-12-29 12:01:39 -0800

    testing change

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved</p>
 </footer>
 </div>
  <?php wp_footer(); ?>
